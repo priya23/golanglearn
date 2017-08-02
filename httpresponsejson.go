11984712449f30df10e4e43dfc3beed70084f93d
priyanka353 <priyanka353rocks@gmail.com>
2017-08-03 00:04:14 +0530

    json related changes

--- httpresponsejson.go
+++ httpresponsejson.go
@@ -2,8 +2,24 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
+	"encoding/json"
 )
-
+type Profile struct {
+	Name string
+	Hobbies []string
+}
 func main(){
-	url := "http://localhost"
+	url := "http://localhost:3000"
+	res,err := http.Get(url)
+	if err != nil{
+		panic(err.Error())
+	}
+	body,err := ioutil.ReadAll(res.Body)
+	var s =new(Profile)
+	errr := json.Unmarshal(body,&s)
+	if errr != nil{
+		panic(errr.Error())
+	}
+	fmt.Println(s)
 }
--- httpserver.go
+++ httpserver.go
@@ -1,49 +1,28 @@
-package main
+package main 
 
 import (
-	//"io"
-	"log"
-	"fmt"
-	"net/http"
 	"encoding/json"
-	"github.com/julienschmidt/httprouter"
+	"net/http"
 )
-type Fruits map[string]int
-type Vegetables map[string]int
-type Payload struct {
-    Stuff Data
-}
-type Data struct {
-    Fruit Fruits
-    Veggies Vegetables
-}
-//func hello(w http.ResponseWriter, r *http.Request) {
-	//io.WriteString(w, "Hello world!")
-//}
-func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-    response, err := getJsonResponse();
-    if err != nil {
-        panic(err)
-    }
-    fmt.Fprintf(w, string(response))
+
+type Profile struct {
+	Name string
+	Hobbies []string
 }
 
-func main() {
-	router := httprouter.New()
-    router.GET("/", Index)
-    log.Fatal(http.ListenAndServe(":8000", router))
+func main(){
+	http.HandleFunc("/",foo)
+	http.ListenAndServe(":3000",nil)
 }
-func getJsonResponse()([]byte, error) {
-    fruits := make(map[string]int)
-    fruits["Apples"] = 25
-    fruits["Oranges"] = 10
 
-    vegetables := make(map[string]int)
-    vegetables["Carrats"] = 10
-    vegetables["Beets"] = 0
+func foo(w http.ResponseWriter, r *http.Request){
+	profile := Profile{"Alex", []string{"snowboarding","programming"}}
+	js,err := json.Marshal(profile)
+	if err != nil {
+		http.Error(w , err.Error(),http.StatusInternalServerError)
+		return
+	}
 
-    d := Data{fruits, vegetables}
-    p := Payload{d}
-
-    return json.MarshalIndent(p, "", "  ")
+	w.Header().Set("Content-Type","application/json")
+	w.Write(js)
 }
